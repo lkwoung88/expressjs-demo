a773673fbe40fc880e981ceaafc275b70a6b0af9
lkwoung88 <lkwoung88@gmail.com>
2025-02-09 01:55:08 +0900

    add route test code and doc

--- src/route/v1/memberRoute.ts
+++ src/route/v1/memberRoute.ts
@@ -14,28 +14,32 @@ const membersMemory = new Map<string, Member>();
 router.get('/:page', (req: Request, res: Response) => {
 
     const page: number = Number.parseInt(req.params.page);
-    res.status(200).send(Array.from(membersMemory.values()));
+    if(isNaN(page)) {
+        res.status(400).json({message: 'Invalid page number'});
+        return;
+    }
+    res.status(200).json({members: Array.from(membersMemory.values())});
 });
 
 router.post('/', (req: Request, res: Response) => {
 
     const newMember: Member = req.body;
     membersMemory.set(newMember.id, newMember);
-    res.status(200).send('Member is created');
+    res.status(200).send({message: 'Member is created'});
 });
 
 router.put('/', (req: Request, res: Response) => {
 
     const updatedMember: Member = req.body;
     membersMemory.set(updatedMember.id, updatedMember);
-    res.status(200).send('Member is updated');
+    res.status(200).send({message: 'Member is updated'});
 });
 
 router.delete('/:id', (req: Request, res: Response) => {
 
     const deletedId = req.params.id;
     membersMemory.delete(deletedId);
-    res.status(200).send('Member is deleted');
+    res.status(200).send({message: 'Member is deleted'});
 });
 
 export default router;
