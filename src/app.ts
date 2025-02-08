198d2732a5ce820cf34129101c44dba8026ab489
lkwoung88 <lkwoung88@gmail.com>
2025-02-08 21:06:52 +0900

    add route

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
-import express, { Express, Request, Response } from 'express';
+import { Express, Request, Response } from 'express';
+import memberRouter from "./route/v1/memberRoute"
 
+const express = require('express');
 const app: Express = express();
 const port = 3000;
+
+app.use(express.json());
+app.use('/api/v1/members', memberRouter);
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, TypeScript with Express! 🎉');
@@ -10,3 +15,5 @@ app.get('/', (req: Request, res: Response) => {
 app.listen(port, () => {
     console.log(`🔥 Server is running at http://localhost:${port}`);
 });
+
+export default app;
